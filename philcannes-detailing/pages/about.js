3fbd56d754d914b01a239248eee47335962fe883
lucas brown <lucas20brown@hotmail.co.uk>
2023-03-24 11:55:25 +0000

    added all pages with head titles

---
+++ philcannes-detailing/pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>PhilCannes - About</title>
+      </Head>
+      <h1>About page</h1>
+    </>
+  )
+}
