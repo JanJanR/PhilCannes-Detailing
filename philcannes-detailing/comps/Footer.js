697dd13b688f6f1bd5c3fd7f0cf0f63f5641602c
John Romero <romerojohn1012@gmail.com>
2023-03-27 14:20:14 +0200

    resolving issue with scss

---
+++ philcannes-detailing/comps/Footer.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_logo">
+        <Image src="/PhilCannes Logo (1) 3.png" width={100} height={100} />
+      </div>
+      <h1>Social Media</h1>
+      <div className="footer_socialMedia">
+
+      </div>
+    </footer>
+   );
+}
+
+export default Footer;
