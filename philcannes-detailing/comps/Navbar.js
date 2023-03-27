0b48f97baf8477ef78c4342585be34852ee47b46
John Romero <romerojohn1012@gmail.com>
2023-03-27 13:16:30 +0200

    added the navbar

---
+++ philcannes-detailing/comps/Navbar.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar_logo">
+       <Image src="/PhilCannes Logo (1) 3.png" width={100} height={100} />
+      </div>
+      <div className="navbar_links">
+        <Link href="/">Home</Link>
+        <Link href ="/about">About Me</Link>
+        <Link href ="/boat-detailing">Boat Detailing</Link>
+        <Link href ="/quote">Get Quote</Link>
+      </div>
+    </nav>
+   );
+}
+
+export default Navbar;
--- philcannes-detailing/pages/index.js
+++ philcannes-detailing/pages/index.js
@@ -1,3 +1,4 @@
+import Navbar from '@/comps/Navbar'
 import Head from 'next/head'
 
 export default function Home() {
